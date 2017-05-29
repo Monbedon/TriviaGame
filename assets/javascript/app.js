6189a82954991b2d020df5c54d0770e9b9e67501
Monbedon <monbedon@gmail.com>
2017-05-29 01:51:11 -0700

    incomplete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,56 @@
- $(document).ready(function() {
+
+ var interValid;
+  
+ var stopWatch = {
+ 	time: 0;
+ 	reset: function(){
+ 		stopWatch.time = 0;
+ 		}
+ 	},
+
+ function run(){
+	interValid = setInterval(decrement, 1000);
+	
+// }
+// 	function decrement(){
+// 	   	timer--;
+// 	   	$("#display").html("<h1>" + stopWatch.time + "</h1>");
+
+// 	    if (stopWatch.time === 0){
+// 	   	stop();
+// 		alert("Time Up!");
+// 	  }
+// 	}
+
+// 	  	function stop(){
+
+// 	   	clearInterval();
+	   	
+// 	   	run();
+	}
 
 
-}
+   	count: function(){
+   		stopWatch.time--;
+
+   		var converted = stopWatch.timeConverter(stopWatch.time);
+
+   		$("#display").html(converted);
+   	},
+
+   
+   timeConverter: function(t){
+	   	var minutes = Math.floor(t/60);
+	   	var seconds = t - (minutes * 60);
+	   	if(seconds < 10){
+	   		seconds = "0" + seconds;
+	   	}
+	   	if (minutes === 0){
+	   		minutes = "00";
+	   	}
+	   	else if (minutes < 10){minutes = "0" + minutes;
+	   	}
+
+	   	return minutes + ":" + seconds;
+	  }
+
